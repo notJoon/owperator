d9bc4e84dddde1a5085c98ef34b3ba2c5b89a4b4
not_joon <lbj199874@gmail.com>
2023-01-25 09:51:41 +0900

    add more error types

---
+++ src/error.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::fmt;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum ParserError {
+    UnexpectedParserError,
+}
+
+impl fmt::Display for ParserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParserError::UnexpectedParserError => write!(f, "Unexpected parser error"),
+        }
+    }
+}
+
+impl Error for ParserError {}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum EvalError {
+    InvalidArgumentHasDetected,
+    InvalidOperatorHasDetected,
+    InvalidConditionType,
+    InvalidLambda,
+    NotEnoughArgs,
+}
+
+impl fmt::Display for EvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EvalError::InvalidArgumentHasDetected => write!(f, "Invalid number of arguments"),
+            EvalError::InvalidOperatorHasDetected => write!(f, "Invalid operator"),
+            EvalError::InvalidConditionType => write!(f, "Condition must be a boolean"),
+            EvalError::InvalidLambda => write!(f, "Invalid Lambda"),
+            EvalError::NotEnoughArgs => write!(f, "Invalid number of arguments"),
+        }
+    }
+}
+
+impl Error for EvalError {}
